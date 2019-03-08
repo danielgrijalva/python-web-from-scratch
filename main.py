7a6650088719a63657d0f61fca9999fc3246ccf2
Daniel Grijalva <jdgs.gt@gmail.com>
2019-03-08 12:01:25 -0700

    Hotfix: return not_found response

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ def application(environ, start_response):
     path = environ.get('PATH_INFO', '').lstrip('/')
     
     callback = get_route(path, urls)
-    return callback(environ, start_response)
+    if callback:
+        return callback(environ, start_response)
+    else:
+        return not_found(environ, start_response)
 
 if __name__ == '__main__':
     server = make_server('localhost', 8000, application)
