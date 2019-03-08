0cbf398f543c6ac69c172e6105c0c29cee3a598d
Daniel Grijalva <jdgs.gt@gmail.com>
2019-03-08 14:26:07 -0700

    Added add and get product views

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from .utils import read_html, get_response_data
+from cgi import parse_qs, escape
+from .models import Model
+
+Product = Model('crud', 'product')
 
 def index(environ, start_response):
-    html = read_html('index')
+
+
+    html = read_html('dashboard')
     status, response_headers = get_response_data(html)
     start_response(status, response_headers)    
 
@@ -15,6 +21,23 @@ def test(environ, start_response):
 
     return [html.encode('utf-8')]
 
+
+def add_product(environ, start_response):
+    html = read_html('add_product')
+    status, response_headers = get_response_data(html)
+    start_response(status, response_headers)
+
+    return [html.encode('utf-8')]
+
+def get_product(environ, start_response):
+    print(parse_qs(environ['QUERY_STRING']))
+    html = read_html('product')
+    status, response_headers = get_response_data(html)
+    start_response(status, response_headers)    
+
+    return [html.encode('utf-8')]
+
+    
 def not_found(environ, start_response):
     html = read_html('404')
     _, response_headers = get_response_data(html)
